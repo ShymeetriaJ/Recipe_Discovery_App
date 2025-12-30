d7dfdf5a9966f8b8c2edbf84f989803675fa0c3d
ShymeetriaJ <sjeducates2@gmail.com>
2025-12-30 00:20:15 -0500

    updated useFetch hook; added try/catch.

--- recipe-discovery-app/recipe-discovery/src/hooks/useFetch.js
+++ recipe-discovery-app/recipe-discovery/src/hooks/useFetch.js
@@ -9,4 +9,39 @@ const useFetch = (url) => {
     // My error state; updater
     const [error, setError] = useState();
 
-}
+    useEffect(() => {
+        console.log('useEffect starting fetch');
+
+        const fetchData = async () => {
+            try {
+                console.log('Fetching data', url);
+
+                const response = await fetch(url);
+                console.log('Received', response);
+
+                if (!response.ok) {
+                    throw new Error(`Fetch error! status: ${response.status}`);
+                }
+
+                const result = await response.json();
+                console.log('Data parsed:', result);
+
+                setData(result);
+                setLoading(false);
+                console.log('Data loading');
+
+            } catch (error) {
+                console.log('Error caught:', error.message);
+                setError(error.message);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [url]);
+
+    return { data, loading, error };
+
+};
+
+export default useFetch;
